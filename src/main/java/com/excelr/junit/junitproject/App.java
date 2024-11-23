0cfcb3f1f81c498384df057f2143e6faa04ac832
diksha847 <dikshaagrawal617@gmail.com>
2024-11-23 22:17:56 +0530

    first commit

---
+++ src/main/java/com/excelr/junit/junitproject/App.java
@@ -0,0 +1,13 @@
+package com.excelr.junit.junitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
